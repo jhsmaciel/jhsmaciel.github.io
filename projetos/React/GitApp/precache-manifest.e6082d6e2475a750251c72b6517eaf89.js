68b3d67acbe4bfdad60586c0bb68e0e89d28ced1
Joao Henrique <joao.maciel@autbank.com.br>
2020-09-24 04:35:17 -0300

    feat(): add react app builded

---
+++ projetos/React/GitApp/precache-manifest.e6082d6e2475a750251c72b6517eaf89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d718e32c8cc06fb303254f190af898",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a85ce1f5544f7a909a78",
+    "url": "/static/js/2.bf17282b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bf17282b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6ace7a217216671e22d",
+    "url": "/static/js/main.f33eb25e.chunk.js"
+  },
+  {
+    "revision": "2a80ce38151084e7cbc0",
+    "url": "/static/js/runtime-main.53235d75.js"
+  },
+  {
+    "revision": "0b819d63338019a0f1cc7263e9c166ae",
+    "url": "/static/media/github-background.0b819d63.svg"
+  },
+  {
+    "revision": "04f6798f67fd4e48d053d05d18d9bd83",
+    "url": "/static/media/github-logo.04f6798f.svg"
+  }
+]);
