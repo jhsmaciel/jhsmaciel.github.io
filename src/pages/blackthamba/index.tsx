20581fb75d9b0d5779966ccd52d69abb06c7fc36
João Maciel <jhsmaciel15@gmail.com>
2025-06-22 21:57:59 -0300

    Refactor BlackThambaLandingPage: replace placeholder sections with structured components (Hero, About, Rules, Events, Gallery, Footer) for improved layout and user experience; update favicon handling and theme management.

--- src/pages/blackthamba/index.tsx
+++ src/pages/blackthamba/index.tsx
@@ -1,47 +1,49 @@
 
 import { useTheme } from 'contexts/ThemeContext';
 import React, { useEffect, useState } from 'react';
+import Hero from './components/hero'; // TODO Translate it to english
+import About from './components/about' 
+import Rules from './components/rules';
+import Events from './components/events';
+import Gallery from './components/gallery';
+import Footer from './components/footer';
+import { Divider } from '@mui/material';
 
 function BlackThambaLandingPage() {
   const { changeTheme, mode } = useTheme();
   const [previousMode] = useState(mode);
 
 
-  const changeFavicon = (imagePath: string) => {
+  const changeFavicon = (image: string) => {
     const favicon = document.querySelector("link[rel~='icon']") as HTMLLinkElement
     if (favicon) {
       let path = process.env.PUBLIC_URL;
-      favicon.href = path + imagePath;
+      console.log(process.env.PUBLIC_URL)
+      favicon.href = path + "/" + image;
     }
   }
 
   useEffect(() => {
     changeTheme('dark');
-    changeFavicon('/blackthamba.png');
+    changeFavicon('blackthamba.png');
     document.title = "Black Thamba";
     return () => {
-      console.log('previousMode', previousMode);
       if (previousMode) changeTheme(previousMode);
-      changeFavicon('/favicon.png');
+      changeFavicon('favicon.png');
       document.title = "João Maciel";
     }
-  }, []);
+  }, [changeTheme, previousMode]);
 
   return (
     <React.Fragment>
-      <div>Black thamba MC</div>
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-      <section id="inicio">Inicio</section>
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-      <section id="sobre">Sobre</section>
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-      <section id="estatuto">Estatuto</section>
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-      <section id="eventos">Eventos</section>
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-      <section id="galeria">Galeria</section>
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-      <section id="contato">Contato</section>
+      <Hero />
+      <Divider />
+      <About />
+      <Rules />
+      <Events />
+      <Gallery />
+      <Divider />
+      <Footer />
     </React.Fragment>
   );
 }
